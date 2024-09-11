b97e524a07cd6dcd50153638ce346369a7731afa
tracer-ics2024 <tracer@njuics.org>
2024-09-11 22:39:30 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  22:39:30 up  2:19,  2 users,  load average: 0.43, 0.29, 0.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -187,7 +187,6 @@ word_t eval(int start, int end, bool *success) {
 		/* Single integer token */
 		word_t result;
 		sscanf(tokens[start].str, "%u", &result); 
-		printf("%u", result);
 		return result;
 	} else if(check_parentheses(start, end) == true) {
 		/* throw away the parentheses */
