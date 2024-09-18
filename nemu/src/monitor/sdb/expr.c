92b7d9ac571d18b52618abe65b6181de5c341475
tracer-ics2024 <tracer@njuics.org>
2024-09-18 14:27:25 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  14:27:25 up 30 min,  2 users,  load average: 0.22, 0.25, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -184,7 +184,6 @@ word_t eval(int start, int end, bool *success) {
       sscanf(tokens[start].str, "%x", &result);
     } else if(tokens[start].type == TK_REG) {
       result = isa_reg_str2val(tokens[start].str, success);
-      printf("%s\n",tokens[start].str);
       if(success == false) {
         printf("No such register.\n");
       }
@@ -263,6 +262,8 @@ word_t eval(int start, int end, bool *success) {
 }
 
 word_t expr(char *e, bool *success) {
+  memset(tokens, 0, TOKEN_SIZE * sizeof(Token));
+
 	if (!make_token(e)) {
 		*success = false;
 		return 0;
