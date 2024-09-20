dbd4cbfe5521c0d8bb44c27483ef89b7df01b1f0
tracer-ics2024 <tracer@njuics.org>
2024-09-20 11:55:48 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  11:55:48 up  1:50,  2 users,  load average: 0.24, 0.31, 0.34

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
-  if (check_wp() == 1) {
+  if (check_wp() == 1 && nemu_state.state != NEMU_END) {
     nemu_state.state = NEMU_STOP;
   }
 #endif
