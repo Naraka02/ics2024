f9d2ef94963eb94b5d1cf16039cfbe41684149ab
tracer-ics2024 <tracer@njuics.org>
2024-10-08 16:30:57 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  16:30:57 up 35 min,  2 users,  load average: 0.30, 0.30, 0.39

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,53 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int ans, len;
+  va_list ap;
+  char buf[1024];
+
+  ans = 0;
+  len = 0;
+  va_start(ap, fmt);
+  for (const char* p = fmt; *p != '\0'; ++p) {
+    switch (*p) {
+      case '%':
+        switch (*++p) {
+          case 'd':
+            int num = va_arg(ap, int); 
+            int num_len = 0;
+            if (num < 0) {
+              buf[len++] = '-';
+              num = -num;
+            }
+            int num_copy = num;
+            do {
+              num_copy /= 10;
+              num_len++;
+            } while (num_copy);
+            buf[len + num_len] = '\0';
+            do {
+              buf[len + --num_len] = num % 10 + '0';
+              num /= 10;
+            } while (num);
+            len += num_len;
+            break;
+          case 's':
+            const char* str = va_arg(ap, const char*);
+            while (*str) {
+              buf[len++] = *str++;
+            }
+            break;
+        }
+        break;
+      default:
+        buf[len++] = *p;
+        break;
+    }
+  }
+  memcpy(out, buf, len);
+  va_end(ap);
+
+  return ans;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
