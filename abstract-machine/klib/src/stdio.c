ba86c4d2a97646434d700e49b0c2732b52873b1d
tracer-ics2024 <tracer@njuics.org>
2024-10-18 00:22:34 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 04 Oct 2024 21:51:11 +0000 x86_64 GNU/Linux  00:22:34 up  2:07,  2 users,  load average: 0.43, 0.57, 0.48

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,49 +5,63 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int print_int(int num, char *buf) {
+  int num_len = 0;
+  int is_neg = num < 0;
+  if (is_neg) {
+    buf[0] = '-';
+    buf++;
+  }
+  int tmp = num;
+  do {
+    tmp /= 10;
+    num_len++;
+  } while (tmp);
+  int digit;
+  for (int i = num_len - 1; i >= 0; --i) {
+    digit = num % 10 < 0 ? -(num % 10) : num % 10;
+    buf[i] = digit + '0';
+    num /= 10;
+  }
+  return num_len + is_neg;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int print_hex(int num, char *buf) {
+  int num_len = 0;
+  int tmp = num;
+  do {
+    tmp /= 16;
+    num_len++;
+  } while (tmp);
+  int digit;
+  for (int i = num_len - 1; i >= 0; --i) {
+    digit = num % 10;
+    buf[i] = digit < 10 ? digit + '0' : digit - 10 + 'a';
+    num /= 10;
+  }
+  return num_len + num < 0; 
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  int ans, len;
-  va_list ap;
-  char buf[1024];
-
-  ans = 0;
-  len = 0;
-  va_start(ap, fmt);
-  for (const char* p = fmt; *p != '\0'; ++p) {
+int _Printf(char *buf, const char *fmt, va_list ap) {
+  int len = 0;
+  for (const char* p = fmt; *p != '\0'; p++) {
     switch (*p) {
       case '%':
         switch (*++p) {
           case 'd':
-            int num = va_arg(ap, int); 
-            int num_len = 0;
-            if (num < 0) {
-              buf[len++] = '-';
-              num = -num;
-            }
-            int num_copy = num;
-            do {
-              num_copy /= 10;
-              num_len++;
-            } while (num_copy);
-            for (int i = 0; i < num_len; ++i) {
-              buf[len + num_len - i - 1] = num % 10 + '0';
-              num /= 10;
-            }
-            len += num_len;
+            len += print_int(va_arg(ap, int), buf + len);
             break;
           case 's':
             const char* str = va_arg(ap, const char*);
             while (*str != '\0') {
               buf[len++] = *str++;
             }
+            break;
+          case 'c':
+            buf[len++] = va_arg(ap, int);
+            break;
+          case 'x':
+            print_hex(va_arg(ap, unsigned int), buf + len);
             break;
         }
         break;
@@ -56,16 +70,53 @@ int sprintf(char *out, const char *fmt, ...) {
         break;
     }
   }
-
   buf[len++] = '\0';
-  memcpy(out, buf, len);
+  return 0;
+}
+
+int printf(const char *fmt, ...) {
+  int ans;
+  va_list ap;
+  char buf[1024];
+
+  va_start(ap, fmt);
+  ans = _Printf(buf, fmt, ap);
+  for (int i = 0; buf[i] != '\0'; ++i) {
+    putch(buf[i]);
+  }
+  va_end(ap);
+
+  return ans;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  int ans;
+  va_list ap;
+  char buf[1024];
+
+  va_start(ap, fmt);
+  ans = _Printf(buf, fmt, ap);
+  memcpy(out, buf, strlen(buf) + 1);
   va_end(ap);
 
   return ans;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  int ans;
+  va_list ap;
+  char buf[1024];
+
+  va_start(ap, fmt);
+  ans = _Printf(buf, fmt, ap);
+  memcpy(out, buf, n);
+  va_end(ap);
+
+  return ans;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
