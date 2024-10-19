10e86ec3c514b21c0fd1104d217711d78cfd9d35
tracer-ics2024 <tracer@njuics.org>
2024-10-19 19:33:48 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 04 Oct 2024 21:51:11 +0000 x86_64 GNU/Linux  19:33:48 up  3:27,  2 users,  load average: 0.26, 0.88, 0.96

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -20,7 +19,6 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  printf("samples = %d\n", ctrl->samples);
   outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
   __am_audio_init();
@@ -38,9 +36,9 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   while (len > sbuf_size - count) {
     count = io_read(AM_AUDIO_STATUS).count;
   }
-  uintptr_t addr = (uintptr_t)AUDIO_ADDR + io_read(AM_AUDIO_STATUS).count;
+  outl(AUDIO_COUNT_ADDR, count + len);
+  uintptr_t addr = AUDIO_SBUF_ADDR + count;
   for (int i = 0; i < len; i++) {
     outb(addr + i, *(uint8_t *)(buf.start + i));
   }
-  outl(AUDIO_COUNT_ADDR, io_read(AM_AUDIO_STATUS).count + len);
 }
