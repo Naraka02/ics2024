b18d394ac54e5865a100518cd439205509562af3
tracer-ics2024 <tracer@njuics.org>
2024-10-19 16:28:25 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 04 Oct 2024 21:51:11 +0000 x86_64 GNU/Linux  16:28:25 up 22 min,  2 users,  load average: 0.65, 0.35, 0.29

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-char *addr;
+char *hbrk;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -35,17 +35,19 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  
+  panic("Not implemented!");
 #endif
-  if (addr == NULL) {
-    addr = (void *)ROUNDUP(heap.start, 8);
+  if (hbrk == NULL) {
+    hbrk = (char *)ROUNDUP(heap.start, 8);
   }
   size = (size_t)ROUNDUP(size, 8);
-  char *old = addr;
-  addr += size;
-  assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
+  char *old = hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+    *p = 0;
+  }
   return old;
-  return NULL;
 }
 
 void free(void *ptr) {
