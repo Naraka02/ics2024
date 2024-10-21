3c884595b68ab8574541963df6b62d8a032c6424
tracer-ics2024 <tracer@njuics.org>
2024-10-22 01:48:28 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.2-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 04 Oct 2024 21:51:11 +0000 x86_64 GNU/Linux  01:48:28 up  6:16,  2 users,  load average: 0.35, 0.66, 0.67

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -31,18 +31,19 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
 static void sdl_audio_callback(void *userdata, uint8_t *stream, int len) {
-  SDL_memset(stream, 0, len);
   int count = audio_base[reg_count];
-  printf("audio count: %d\n", count);
-  printf("audio len: %d\n", len);
-  if (count > len) {
-    SDL_memcpy(stream, sbuf, len);
-    audio_base[reg_count] -= len;
-    SDL_memcpy(sbuf, sbuf + len, count - len);
-  } else {
-    SDL_memcpy(stream, sbuf, count);
+  if (count == 0) {
+    SDL_memset(stream, 0, len);
+    return;
+  }
+  if (count < len) {
+    count = len;
+  }
+  SDL_memcpy(stream, sbuf, count);
+  sbuf += count;
+  audio_base[reg_count] -= count;
+  if (count < len) {
     SDL_memset(stream + count, 0, len - count);
-    audio_base[reg_count] = 0;
   }
 }
 
