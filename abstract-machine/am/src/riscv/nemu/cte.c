0e2f8385faa13abd7e32b247b730589c64df92c0
tracer-ics2024 <tracer@njuics.org>
2024-10-27 01:16:56 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  01:16:56 up 29 min,  2 users,  load average: 0.51, 0.42, 0.41

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,6 +5,9 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  printf("mcause: 0x%x\n", c->mcause);
+  printf("mstatus: 0x%x\n", c->mstatus);
+  printf("mepc: 0x%x\n", c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -14,9 +17,6 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  printf("mcause: 0x%x\n", c->mcause);
-  printf("mstatus: 0x%x\n", c->mstatus);
-  printf("mepc: 0x%x\n", c->mepc);
 
   return c;
 }
