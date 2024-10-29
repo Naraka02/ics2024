a48e46030072db700f8053c32a9eae9043d6a598
tracer-ics2024 <tracer@njuics.org>
2024-10-29 16:28:45 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  16:28:45 up 36 min,  2 users,  load average: 0.13, 0.27, 0.36

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,6 +19,11 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+#ifdef ETRACE
+  Log("Interrupt NO = %d", NO);
+  Log("Interrupt epc = 0x%x", epc);
+  Log("Interrupt mtvec = 0x%x", cpu.csr.mtvec);
+#endif
   cpu.csr.mepc = epc;
   cpu.csr.mcause = NO;
   return cpu.csr.mtvec;
