c993f41aa54d27a680d69677d49f6b41f75a12ea
tracer-ics2024 <tracer@njuics.org>
2024-10-31 17:01:48 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  17:01:48 up 57 min,  2 users,  load average: 0.49, 0.57, 0.49

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,7 +18,19 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
   assert(ehdr.e_ident[0] == 0x7f && ehdr.e_ident[1] == 'E' && ehdr.e_ident[2] == 'L' && ehdr.e_ident[3] == 'F');
-  return 0;
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phnum * ehdr.e_phentsize);
+
+  uintptr_t ret = 0;
+  for (int i = 0; i< ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read(&phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      memset(&(phdr[i].p_paddr) + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+      ret = phdr[i].p_vaddr;
+    }
+  }
+  return ret;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
