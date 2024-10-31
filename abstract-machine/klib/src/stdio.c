f48baeed578636d9abf9a123b49c44adb25efdd7
tracer-ics2024 <tracer@njuics.org>
2024-10-31 17:46:37 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  17:46:37 up  1:42,  2 users,  load average: 0.37, 0.42, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -54,7 +54,7 @@ int _Printf(char *buf, const char *fmt, va_list ap) {
             len += print_int(va_arg(ap, int), buf + len);
             break;
           case 's':
-            const char* str = va_arg(ap, const char*);
+            char *str = va_arg(ap, char*);
             while (*str != '\0') {
               buf[len++] = *str++;
             }
@@ -68,7 +68,7 @@ int _Printf(char *buf, const char *fmt, va_list ap) {
           case 'p':
             buf[len++] = '0';
             buf[len++] = 'x';
-            print_hex(va_arg(ap, unsigned), buf + len);
+            print_hex(va_arg(ap, unsigned int), buf + len);
             break;
         }
         break;
