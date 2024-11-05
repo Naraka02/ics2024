2cacfea8df3cb3cdfd60900f97dc24f6394d84a1
tracer-ics2024 <tracer@njuics.org>
2024-11-05 17:21:53 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  17:21:53 up  1:12,  2 users,  load average: 0.74, 0.51, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,12 +1,12 @@
-#include <proc.h>
 #include <elf.h>
+#include <proc.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
@@ -15,25 +15,24 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf32_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
-  assert(ehdr.e_ident[0] == 0x7f && ehdr.e_ident[1] == 'E' && ehdr.e_ident[2] == 'L' && ehdr.e_ident[3] == 'F');
+  assert(ehdr.e_ident[0] == 0x7f && ehdr.e_ident[1] == 'E' &&
+         ehdr.e_ident[2] == 'L' && ehdr.e_ident[3] == 'F');
 
   Elf32_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phnum * ehdr.e_phentsize);
 
-  uintptr_t ret = 0;
-  for (int i = 0; i< ehdr.e_phnum; i++) {
+  for (int i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
       ramdisk_read(&phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
-      memset(&(phdr[i].p_vaddr) + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
-      ret = phdr[i].p_vaddr;
+      memset(&(phdr[i].p_vaddr) + phdr[i].p_filesz, 0,
+             phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
-  return ret;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
