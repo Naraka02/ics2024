8c21696bce38e2afc42b79403c6e766cfda8ab12
tracer-ics2024 <tracer@njuics.org>
2024-11-05 17:40:57 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  17:40:57 up  1:31,  2 users,  load average: 0.25, 0.30, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,14 +10,26 @@
 #define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+#define EXPECT_TYPE EM_386
+#elif defined(__ISA_RISCV32__)
+#define EXPECT_TYPE EM_RISCV
+#else
+#error Unsupported ISA
+#endif
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf32_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(ehdr));
+
   assert(ehdr.e_ident[0] == 0x7f && ehdr.e_ident[1] == 'E' &&
          ehdr.e_ident[2] == 'L' && ehdr.e_ident[3] == 'F');
+  assert(ehdr.e_machine == EXPECT_TYPE);
 
   Elf32_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phnum * ehdr.e_phentsize);
