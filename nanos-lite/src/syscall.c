88e64c650058732dfed9dd73a9b9695882165fe7
tracer-ics2024 <tracer@njuics.org>
2024-11-06 16:02:50 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  16:02:50 up 3 min,  2 users,  load average: 0.35, 0.24, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,9 +2,24 @@
 #include "am.h"
 #include <common.h>
 
+typedef signed long ssize_t;
+
 static inline void sys_yield(Context *c) {
   yield();
   c->GPRx = 0;
+}
+
+static inline ssize_t sys_write(int fd, const void *buf, size_t count) {
+  switch (fd) {
+  case 1:
+  case 2:
+    for (size_t i = 0; i < count; i++) {
+      putch(((char *)buf)[i]);
+    }
+    return count;
+  default:
+    return -1;
+  }
 }
 
 void do_syscall(Context *c) {
@@ -20,6 +35,9 @@ void do_syscall(Context *c) {
     break;
   case SYS_exit:
     halt(0);
+    break;
+  case SYS_write:
+    c->GPRx = sys_write(a[1], (void *)a[2], a[3]);
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
