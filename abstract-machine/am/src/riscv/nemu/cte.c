f1376319fdb5e82b16d5943fb2728cd0fec4ef1f
tracer-ics2024 <tracer@njuics.org>
2024-11-06 15:16:20 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:16:20 up  1:14,  2 users,  load average: 0.05, 0.24, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,6 +10,7 @@ Context *__am_irq_handle(Context *c) {
     printf("mcause : %d\n", c->mcause);
     switch (c->mcause) {
     case 1:
+    case 0:
       ev.event = EVENT_SYSCALL;
       break;
     case -1:
