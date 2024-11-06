4f7296e70b51612ae684c3b81f2f94ad0a67c775
tracer-ics2024 <tracer@njuics.org>
2024-11-06 14:41:43 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  14:41:43 up 39 min,  2 users,  load average: 0.36, 0.38, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -36,11 +36,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   for (int i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
-      printf("Loading %d bytes to 0x%x\n", phdr[i].p_filesz, phdr[i].p_vaddr);
       ramdisk_read((void *)(uintptr_t)phdr[i].p_vaddr, phdr[i].p_offset,
                    phdr[i].p_filesz);
-      printf("Clearing %d bytes at 0x%x\n", phdr[i].p_memsz - phdr[i].p_filesz,
-             phdr[i].p_vaddr + phdr[i].p_filesz);
       memset((void *)(uintptr_t)(phdr[i].p_vaddr + phdr[i].p_filesz), 0,
              phdr[i].p_memsz - phdr[i].p_filesz);
     }
