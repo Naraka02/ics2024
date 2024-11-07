7bbcda4f72eb7f3dbc28d6ce858bbbeea3bb0c44
tracer-ics2024 <tracer@njuics.org>
2024-11-07 16:17:09 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  16:17:09 up 21 min,  2 users,  load average: 0.13, 0.25, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -40,6 +40,7 @@ static inline int sys_brk(int *addr) {
   if (addr > (int *)brk) {
     brk = (char *)addr;
   }
+  printf("brk: %p\n", brk);
   return (int)old_brk;
 }
 
@@ -63,8 +64,6 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
-  Log("Syscall ID = %d", a[0]);
 
   switch (a[0]) {
   case SYS_yield:
