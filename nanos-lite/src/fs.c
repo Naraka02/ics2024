8ac65d41df921ef98834cb3683b0399e025e791f
tracer-ics2024 <tracer@njuics.org>
2024-11-07 16:38:18 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  16:38:18 up 42 min,  2 users,  load average: 0.44, 0.27, 0.26

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -48,6 +48,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
+  Log("fs_read: offset = %d, len = %d", open_offset, len);
   assert(open_offset + len <= file_table[fd].size);
   open_offset += len;
   return file_table[fd].read(
