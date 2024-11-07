fce8da25dfd8e3fe1688b88207a856a4d5c39b3d
tracer-ics2024 <tracer@njuics.org>
2024-11-07 15:40:43 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:40:43 up  1:44,  2 users,  load average: 0.42, 0.32, 0.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -8,15 +8,14 @@ PCB *current = NULL;
 
 void naive_uload(PCB *pcb, const char *filename);
 
-void switch_boot_pcb() {
-  current = &pcb_boot;
-}
+void switch_boot_pcb() { current = &pcb_boot; }
 
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
-    j ++;
+    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!",
+        (uintptr_t)arg, j);
+    j++;
     yield();
   }
 }
@@ -26,10 +25,7 @@ void init_proc() {
 
   Log("Initializing processes...");
 
-  naive_uload(NULL, NULL);
-
+  naive_uload(NULL, "bin/file-test");
 }
 
-Context* schedule(Context *prev) {
-  return NULL;
-}
+Context *schedule(Context *prev) { return NULL; }
