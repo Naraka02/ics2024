961a67fb83f4bf6d46ec0f4e50f1732edd05e738
tracer-ics2024 <tracer@njuics.org>
2024-11-07 17:12:47 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  17:12:47 up  1:16,  2 users,  load average: 1.29, 0.87, 0.57

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -7,6 +7,7 @@ int main() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   long us = tv.tv_usec;
+  printf("us = %ld\n", us);
   while (1) {
     gettimeofday(&tv, NULL);
     if (tv.tv_usec - us >= 500000) {
