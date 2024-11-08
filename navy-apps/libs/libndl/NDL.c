6b1dc2aaf815ccf54c966fb8b620e9ff89c6cf42
tracer-ics2024 <tracer@njuics.org>
2024-11-08 15:44:17 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:44:17 up 22 min,  2 users,  load average: 0.14, 0.18, 0.13

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -64,8 +64,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0);
   int nx = screen_w - w / 2, ny = screen_h - h / 2;
   for (int j = 0; j < h; j++) {
-    lseek(fd, (y + j) * screen_w * 4 + x * 4, SEEK_SET);
-    write(fd, pixels + j * w, w * 4);
+    lseek(fd, (y + j) * screen_w + x, SEEK_SET);
+    write(fd, pixels + j * w, w);
   }
   close(fd);
 }
