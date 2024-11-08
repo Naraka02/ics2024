689f8a601846ca05c5f071c534c523908885d406
tracer-ics2024 <tracer@njuics.org>
2024-11-08 14:40:50 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  14:40:50 up 54 min,  2 users,  load average: 0.31, 0.29, 0.26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,6 +23,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   if (ev.keycode == AM_KEY_NONE) {
     return 0;
   }
+  Log("keycode = %d, keydown = %d", ev.keycode, ev.keydown);
   return snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku",
                   keyname[ev.keycode]);
 }
