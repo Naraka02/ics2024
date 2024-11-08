98da5a4a89670e769f526d41e2c22b689cc90269
tracer-ics2024 <tracer@njuics.org>
2024-11-08 14:33:51 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  14:33:51 up 47 min,  2 users,  load average: 0.18, 0.17, 0.22

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,7 +18,6 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  printf("NDL_PollEvent");
   int fp = open("/dev/events", 0);
   int ret = read(fp, buf, len);
   close(fp);
