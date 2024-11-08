c9e9a8620f67e1a45975d6a1faae9cd5aab9bb3b
tracer-ics2024 <tracer@njuics.org>
2024-11-08 15:36:49 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:36:49 up 15 min,  2 users,  load average: 0.37, 0.22, 0.12

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -63,19 +63,13 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0);
   int center_x = screen_w / 2, center_y = screen_h / 2;
-  int nx = x + w, ny = y + h;
-  if (nx > screen_w)
-    nx = screen_w;
-  if (ny > screen_h)
-    ny = screen_h;
-  for (int i = y; i < ny; i++) {
-    for (int j = x; j < nx; j++) {
-      if (i >= 0 && i < screen_h && j >= 0 && j < screen_w) {
-        int pos = i * screen_w + j;
-        lseek(fd, pos * 4, SEEK_SET);
-        write(fd, &pixels[pos], 4);
-      }
-    }
+  int dx = x - center_x, dy = y - center_y;
+  int sx = dx < 0 ? -dx : 0, sy = dy < 0 ? -dy : 0;
+  int ex = dx + w > screen_w ? screen_w - dx : w;
+  int ey = dy + h > screen_h ? screen_h - dy : h;
+  for (int i = sy; i < ey; i++) {
+    lseek(fd, (dy + i) * screen_w * 4 + (dx + sx) * 4, SEEK_SET);
+    write(fd, pixels + i * w + sx, ex * 4);
   }
   close(fd);
 }
