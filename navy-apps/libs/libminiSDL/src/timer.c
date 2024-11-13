56f5f2bac00c7d02250465b0241d0b84b4976f06
tracer-ics2024 <tracer@njuics.org>
2024-11-14 01:07:01 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 17:57:56 +0000 x86_64 GNU/Linux  01:07:01 up  4:04,  2 users,  load average: 0.38, 0.46, 0.44

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -13,6 +13,7 @@ uint32_t SDL_GetTicks() { return NDL_GetTicks(); }
 
 void SDL_Delay(uint32_t ms) {
   uint32_t start = NDL_GetTicks();
-  while ((NDL_GetTicks() - start) < ms)
-    ;
+  while ((NDL_GetTicks() - start) < ms) {
+    printf("%d", NDL_GetTicks());
+  }
 }
