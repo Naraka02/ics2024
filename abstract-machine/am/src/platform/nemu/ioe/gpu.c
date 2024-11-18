3c7048cc3b8728891257909201a6a72bbf728421
tracer-ics2024 <tracer@njuics.org>
2024-11-18 23:24:04 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 08 Nov 2024 17:57:56 +0000 x86_64 GNU/Linux  23:24:04 up  1:04,  2 users,  load average: 0.86, 1.16, 1.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,23 +1,26 @@
 #include <am.h>
+#include <klib.h>
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t screen_info = inl(VGACTL_ADDR);
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = screen_info >> 16, .height = screen_info & 0xffff,
-    .vmemsz = (screen_info >> 16) * (screen_info & 0xffff) * 4,
+  *cfg = (AM_GPU_CONFIG_T){
+      .present = true,
+      .has_accel = false,
+      .width = screen_info >> 16,
+      .height = screen_info & 0xffff,
+      .vmemsz = (screen_info >> 16) * (screen_info & 0xffff) * 4,
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  printf("%d\n", x);
   uint32_t *pixels = ctl->pixels;
   uint32_t screen_width = inl(VGACTL_ADDR) >> 16;
   for (int j = 0; j < h; j++) {
@@ -30,6 +33,4 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
