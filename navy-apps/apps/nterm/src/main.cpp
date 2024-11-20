3dd6b37e88e0847a6b869fc8e456ea00ebca1895
tracer-ics2024 <tracer@njuics.org>
2024-11-20 16:25:58 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  16:25:58 up 31 min,  2 users,  load average: 0.24, 0.56, 0.51

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   SDL_Init(0);
   font = new BDF_Font(font_fname);
 
-  assert(0);
+  printf("font: %d x %d\n", font->w, font->h);
   // setup display
   int win_w = font->w * W;
   int win_h = font->h * H;
