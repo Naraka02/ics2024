a6b27260f78bc3d18603019cab28524b6350d354
tracer-ics2024 <tracer@njuics.org>
2024-11-20 16:07:47 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  16:07:47 up 13 min,  2 users,  load average: 0.74, 0.55, 0.33

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
 
   Log("Initializing processes...");
 
-  naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/bin/pal");
 }
 
 Context *schedule(Context *prev) { return NULL; }
