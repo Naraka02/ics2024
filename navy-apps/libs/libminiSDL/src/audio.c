3d778923ffc4086b80e0b15c19f8c9d03ef747dc
tracer-ics2024 <tracer@njuics.org>
2024-11-20 17:03:43 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  17:03:43 up  1:09,  2 users,  load average: 0.31, 0.23, 0.28

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,28 +1,48 @@
 #include <NDL.h>
 #include <SDL.h>
 
+static uint32_t timer = 0;
+static uint32_t interval = 0;
+static uint8_t *stream = NULL;
+static int samples = 0;
+static void (*SDL_Callback)(void *, uint8_t *, int) = NULL;
+
+void CallBackHelper() {
+  if (SDL_GetTicks() - timer < interval)
+    return;
+  timer = SDL_GetTicks();
+  SDL_Callback(NULL, stream, samples);
+  NDL_PlayAudio(stream, samples);
+}
+
 int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
+  interval = 1000 / desired->freq;
+  stream = (uint8_t *)malloc(desired->samples);
+  samples = desired->samples;
+  SDL_Callback = desired->callback;
+  NDL_OpenAudio(desired->freq, desired->channels, desired->samples);
+
+  CallBackHelper();
   return 0;
 }
 
-void SDL_CloseAudio() {
-}
+void SDL_CloseAudio() {}
 
 void SDL_PauseAudio(int pause_on) {
+  if (pause_on > 0)
+    return;
+  CallBackHelper();
 }
 
-void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
-}
+void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {}
 
-SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
+SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec,
+                           uint8_t **audio_buf, uint32_t *audio_len) {
   return NULL;
 }
 
-void SDL_FreeWAV(uint8_t *audio_buf) {
-}
+void SDL_FreeWAV(uint8_t *audio_buf) {}
 
-void SDL_LockAudio() {
-}
+void SDL_LockAudio() {}
 
-void SDL_UnlockAudio() {
-}
+void SDL_UnlockAudio() {}
