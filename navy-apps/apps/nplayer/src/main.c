8b2b88582c128a9f0a27c766ed9fed62f5e3e39b
tracer-ics2024 <tracer@njuics.org>
2024-11-21 16:17:15 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  16:17:15 up 8 min,  2 users,  load average: 0.53, 0.31, 0.15

--- navy-apps/apps/nplayer/src/main.c
+++ navy-apps/apps/nplayer/src/main.c
@@ -79,8 +79,8 @@ void FillAudio(void *userdata, uint8_t *stream, int len) {
   }
   if (nbyte < len)
     memset(stream + nbyte, 0, len - nbyte);
-  printf("Playing %d samples\n", samples_per_channel);
   memcpy(stream_save, stream, len);
+  printf("Playing %d samples\n", samples_per_channel);
 }
 
 int main(int argc, char *argv[]) {
