07c39e2d9ec5b7bdb085f469ae33c73e5731dacb
tracer-ics2024 <tracer@njuics.org>
2024-11-21 23:07:21 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.11.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 17 Nov 2024 16:06:17 +0000 x86_64 GNU/Linux  23:07:21 up  1:40,  2 users,  load average: 0.38, 0.38, 0.35

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,21 +1,21 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <SDL2/SDL.h>
 #include <common.h>
 #include <device/map.h>
-#include <SDL2/SDL.h>
 
 enum {
   reg_freq,
@@ -60,14 +60,14 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset < nr_reg * sizeof(uint32_t) && offset % 4 == 0);
   int idx = offset / 4;
   switch (idx) {
-    case reg_init:
-      if (is_write && audio_base[reg_init] == 1) {
-        init_sdl();
-        audio_base[reg_init] = 0;
-      }
-      break;
-    default:
-      break;
+  case reg_init:
+    if (is_write && audio_base[reg_init] == 1) {
+      init_sdl();
+      audio_base[reg_init] = 0;
+    }
+    break;
+  default:
+    break;
   }
 }
 
@@ -75,12 +75,15 @@ void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
   audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  printf("audio: sbuf size = %d\n", audio_base[reg_sbuf_size]);
   audio_base[reg_init] = 1;
   audio_base[reg_count] = 0;
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+  add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size,
+              audio_io_handler);
 #else
-  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
+  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size,
+               audio_io_handler);
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
