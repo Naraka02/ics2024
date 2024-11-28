cacc7aa6c43a04b516dacd7245efe8d2334a982c
tracer-ics2024 <tracer@njuics.org>
2024-11-28 16:39:34 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  16:39:34 up 42 min,  2 users,  load average: 1.80, 1.48, 1.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -40,7 +40,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   memset(c, 0, sizeof(Context));
   c->mstatus = 0x1800;
   c->mepc = (uintptr_t)entry;
-  c->GPRx = (uintptr_t)arg;
+  c->gpr[10] = (uintptr_t)arg;
   return c;
 }
 
