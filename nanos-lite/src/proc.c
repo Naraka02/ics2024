c96bb8578b1ac1082f0da8b6d73c4be6826b237e
tracer-ics2024 <tracer@njuics.org>
2024-11-29 11:39:35 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  11:39:35 up  1:28,  2 users,  load average: 1.04, 0.85, 0.90

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -7,7 +7,8 @@ static PCB pcb_boot = {};
 PCB *current = NULL;
 
 void naive_uload(PCB *pcb, const char *filename);
-void context_uload(PCB *pcb, const char *filename);
+void context_uload(PCB *pcb, const char *filename, char *const argv,
+                   char *const envp);
 
 void switch_boot_pcb() { current = &pcb_boot; }
 
@@ -28,7 +29,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0);
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[1], "/bin/pal", "--skip", "");
   switch_boot_pcb();
 
   // Log("Initializing processes...");
