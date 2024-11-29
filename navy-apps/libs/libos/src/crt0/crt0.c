5d01686f676d491ef44b85df1268f4de06fb9407
tracer-ics2024 <tracer@njuics.org>
2024-11-29 15:06:41 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  15:06:41 up  1:08,  2 users,  load average: 1.49, 1.33, 1.32

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
@@ -10,9 +11,10 @@ void call_main(uintptr_t *args) {
   printf("args = %p\n", args);
   printf("argc = %d\n", argc);
   printf("%s\n", (char *)args + 4);
-  char **argv = (char **)(args + 1);
-  for (int i = 0; i <= argc; i++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
+  char **argv = malloc(argc * sizeof(char *));
+  argv[0] = (char *)args + 4;
+  for (int i = 1; i < argc; i++) {
+    argv[i] = argv[i - 1] + strlen(argv[i - 1]) + 1;
   }
   char **envp = (char **)args[2];
   environ = envp;
