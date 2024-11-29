ab681daba08ba7e906419128f14bf7046145a119
tracer-ics2024 <tracer@njuics.org>
2024-11-29 11:23:46 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  11:23:46 up  1:12,  2 users,  load average: 1.50, 1.07, 0.98

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,9 +5,11 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] = {NULL};
-  environ = empty;
+  int argc = args[0];
+  char **argv = (char **)args[1];
+  char **envp = (char **)args[2];
+  environ = envp;
   asm("call  __libc_init_array");
-  exit(main(0, empty, empty));
+  exit(main(0, argv, envp));
   assert(0);
 }
