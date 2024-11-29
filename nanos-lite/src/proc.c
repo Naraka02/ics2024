da217881954c464a8014888c6b3ddf51fb1d28be
tracer-ics2024 <tracer@njuics.org>
2024-11-29 14:45:11 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  14:45:11 up 46 min,  2 users,  load average: 1.46, 1.44, 1.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0);
-  char *argv[] = {"--skip"};
+  char **argv = {};
   char **envp = {};
   context_uload(&pcb[1], "/bin/pal", argv, envp);
   switch_boot_pcb();
