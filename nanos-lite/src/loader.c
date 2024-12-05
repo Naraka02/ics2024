f2f822f31f69b56cd5d370891ff8a86077ab3b70
tracer-ics2024 <tracer@njuics.org>
2024-12-05 17:34:43 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 16:04:27 +0000 x86_64 GNU/Linux  17:34:43 up  1:37,  2 users,  load average: 0.64, 0.48, 0.39

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -58,13 +58,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[],
                    char *const envp[]) {
+  if (argv) {
+    printf("argv[0] = %s\n", argv[0]);
+  }
   uintptr_t entry = loader(pcb, filename);
   Area kstack = {pcb->stack, pcb->stack + STACK_SIZE};
 
   int argc = 0, envc = 0;
-  if (argv) {
-    printf("argv[0] = %s\n", argv[0]);
-  }
   uintptr_t *sp = new_page(NR_PAGES); // ustack.end
   if (argv) {
     printf("argv[0] = %s\n", argv[0]);
