6ebf3883a48c939e8dc3e012a9d28e7e8dac995a
tracer-ics2024 <tracer@njuics.org>
2024-12-12 17:38:46 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  17:38:46 up  1:30,  2 users,  load average: 0.93, 0.64, 0.64

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -74,7 +74,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 
   PTE *updir = as->ptr;
   PTE *updir_pte = updir + vpn_1;
-  printf("%p\n", updir_pte);
+  printf("%p\n", updir);
 
   if (updir_pte == 0) {
     PTE *dir = (PTE *)(pgalloc_usr(PGSIZE));
