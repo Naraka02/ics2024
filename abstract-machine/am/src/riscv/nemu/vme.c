a943fc25a171378d755e5cb4eb4c8d86471d366b
tracer-ics2024 <tracer@njuics.org>
2024-12-13 10:16:09 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  10:16:09 up 5 min,  2 users,  load average: 0.69, 0.87, 0.42

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -32,7 +32,6 @@ bool vme_init(void *(*pgalloc_f)(int), void (*pgfree_f)(void *)) {
   int i;
   for (i = 0; i < LENGTH(segments); i++) {
     void *va = segments[i].start;
-    printf("hello");
     for (; va < segments[i].end; va += PGSIZE) {
       map(&kas, va, va, 0);
     }
