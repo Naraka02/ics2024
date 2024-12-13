ab37863c2d612f5f13b7a5e317b7da0f55aa31a9
tracer-ics2024 <tracer@njuics.org>
2024-12-13 11:42:41 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  11:42:41 up  1:32,  2 users,  load average: 0.34, 0.36, 0.30

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -17,7 +17,6 @@ static Area segments[] = { // Kernel memory mappings
 
 static inline void set_satp(void *pdir) {
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
-  printf("satp : 0x%x", (mode | (uintptr_t)pdir >> 12));
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
