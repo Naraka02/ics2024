51f88e3d1377e099577ba2f3475e551dc5ae3e89
tracer-ics2024 <tracer@njuics.org>
2024-12-13 15:28:36 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:28:36 up  1:28,  2 users,  load average: 0.75, 0.65, 0.61

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -20,8 +20,7 @@
 #include <stdint.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  if (vaddr < 0x80000000)
-    printf("%x\n", vaddr);
+  printf("%x\n", vaddr);
   uint32_t vpn_1 = BITS(vaddr, 31, 22);
   uint32_t vpn_0 = BITS(vaddr, 21, 12);
   uint32_t page_offset = BITS(vaddr, 11, 0);
