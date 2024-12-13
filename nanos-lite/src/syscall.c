143ecf05a65b8747e65fb33f3378359d4c74238a
tracer-ics2024 <tracer@njuics.org>
2024-12-13 15:49:10 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:49:10 up  1:48,  2 users,  load average: 1.00, 0.76, 0.68

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -69,8 +69,7 @@ static inline int sys_execve(const char *filename, char *const argv[],
 }
 
 static inline int sys_exit(int status) {
-  halt(0);
-  // sys_execve("/bin/nterm", NULL, NULL);
+  sys_execve("/bin/nterm", NULL, NULL);
   return 0;
 }
 
