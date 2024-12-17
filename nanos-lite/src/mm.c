0d712163e5b71f2c2f58b136838a882d623125ef
tracer-ics2024 <tracer@njuics.org>
2024-12-17 17:51:55 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  17:51:55 up  1:41,  2 users,  load average: 0.44, 0.40, 0.37

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -22,8 +22,6 @@ void free_page(void *p) { panic("not implement yet"); }
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
-  printf("heap.start %p heap.end %p\n", heap.start, heap.end);
-  printf("brk %p max_brk %p\n", brk, current->max_brk);
   if (current->max_brk >= brk) {
     return 0;
   }
@@ -35,6 +33,7 @@ int mm_brk(uintptr_t brk) {
   int nr_pages = (int)(brk - current->max_brk - 1) / PGSIZE + 1;
   for (int i = 0; i < nr_pages; i++) {
     void *page = new_page(1);
+    printf("new_page: %p\n", page);
     map(&current->as, (void *)current->max_brk + i * PGSIZE, page, 0b1110);
   }
   current->max_brk += nr_pages * PGSIZE;
