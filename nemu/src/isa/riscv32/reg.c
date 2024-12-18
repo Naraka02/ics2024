82e234c014c1c6608973f86a7eb99a8a50e1b4aa
tracer-ics2024 <tracer@njuics.org>
2024-12-18 16:47:16 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  16:47:16 up 54 min,  2 users,  load average: 0.15, 0.28, 0.25

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,39 +1,38 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
 #include "local-include/reg.h"
+#include <isa.h>
 
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
+const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
+                      "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
+                      "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
+                      "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 const int NR_REGS = sizeof(regs) / sizeof(char *);
 
 void isa_reg_display() {
   int i;
   for (i = 0; i < NR_REGS; i++) {
-    if ( i == 1 || i == 2 || i == 3 || i == 8 || i == 9) {
-		  printf("%-10s\t0x%-10x\t0x%x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+    if (i == 1 || i == 2 || i == 3 || i == 8 || i == 9) {
+      printf("%-10s\t0x%-10x\t0x%x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     } else {
       printf("%-10s\t0x%-10x\t%u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
     }
   }
-	printf("%-10s\t0x%-10x\t0x%x\n", "pc", cpu.pc, cpu.pc);
+  printf("%-10s\t0x%-10x\t0x%x\n", "pc", cpu.pc, cpu.pc);
+  printf("%-10s\t0x%-10x\t0x%x\n", "mstatus", cpu.csr.mstatus, cpu.csr.mstatus);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
