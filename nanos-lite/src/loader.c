3ec41054353cb3aae7f09ad418bc3a2dcbd09425
tracer-ics2024 <tracer@njuics.org>
2024-12-18 15:41:00 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:41:00 up  1:39,  2 users,  load average: 0.41, 0.39, 0.39

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,7 +49,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if (phdr[i].p_type == PT_LOAD) {
       int nr_pages = (phdr[i].p_memsz - 1) / PGSIZE + 1;
       uintptr_t va = phdr[i].p_vaddr & ~(PGSIZE - 1);
-      printf("%x %x\n", phdr[i].p_vaddr, va);
+      printf("%x %x %d\n", phdr[i].p_vaddr, va, phdr[i].p_memsz);
       void *pages = new_page(nr_pages);
 
       fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
