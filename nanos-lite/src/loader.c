cf9274e0f1dbf470d0db607ed2f102fc91335592
tracer-ics2024 <tracer@njuics.org>
2024-12-18 15:07:22 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:07:22 up  1:05,  2 users,  load average: 0.52, 0.33, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -66,6 +66,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   fs_close(fd);
 
   pcb->max_brk = (max_brk + PGSIZE - 1) & ~(PGSIZE - 1);
+  printf("max_brk: %p\n", pcb->max_brk);
   return ehdr.e_entry;
 }
 
