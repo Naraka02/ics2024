2d4753c8435d71ffb07a310f0950bfa4af3706a0
tracer-ics2024 <tracer@njuics.org>
2024-12-19 15:11:17 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:11:17 up  1:14,  2 users,  load average: 0.63, 0.43, 0.33

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,7 +14,6 @@ static Context *do_event(Event e, Context *c) {
     break;
   case EVENT_IRQ_TIMER:
     c = schedule(c);
-    Log("IRQ_TIMER");
     break;
   default:
     panic("Unhandled event ID = %d", e.event);
