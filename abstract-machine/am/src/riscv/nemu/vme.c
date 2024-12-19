71d49b7aa6380410367c679b809a8137479095af
tracer-ics2024 <tracer@njuics.org>
2024-12-19 14:41:59 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  14:41:59 up 44 min,  2 users,  load average: 0.52, 0.42, 0.36

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -49,6 +49,7 @@ bool vme_init(void *(*pgalloc_f)(int), void (*pgfree_f)(void *)) {
 
 void protect(AddrSpace *as) {
   PTE *updir = (PTE *)(pgalloc_usr(PGSIZE));
+  printf("protecting updir %p\n", updir);
   as->ptr = updir;
   as->area = USER_SPACE;
   as->pgsize = PGSIZE;
