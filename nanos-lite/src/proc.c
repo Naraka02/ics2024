761b5a217aafa7275013e750a1042f9a12bac4f8
tracer-ics2024 <tracer@njuics.org>
2024-12-19 14:15:13 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  14:15:13 up 18 min,  2 users,  load average: 0.82, 0.62, 0.38

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void init_proc() {
 
 Context *schedule(Context *prev) {
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   printf("current is %d\n", current == &pcb[0] ? 0 : 1);
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
