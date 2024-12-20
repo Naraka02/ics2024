3681d1ffa26beec06898eb12c940fce4656fc065
tracer-ics2024 <tracer@njuics.org>
2024-12-20 15:49:29 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:49:29 up  1:48,  2 users,  load average: 0.14, 0.36, 0.36

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -97,7 +97,6 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   c->mstatus = 0x00000080;
   c->mepc = (uintptr_t)entry - 4;
   c->pdir = as->ptr;
-  c->GPRx = (uintptr_t)kstack.end;
   c->np = 1;
   return c;
 }
