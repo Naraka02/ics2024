7a4b8f7cfd06fb2008af44902cafb1b5756d3b7d
tracer-ics2024 <tracer@njuics.org>
2024-12-20 15:27:41 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:27:41 up  1:26,  2 users,  load average: 0.27, 0.29, 0.26

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -50,6 +50,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context *)(kstack.end - sizeof(Context));
   memset(c, 0, sizeof(Context));
   c->mstatus = 0x00001880;
+  printf("entry = %p\n", entry);
   c->mepc = (uintptr_t)entry - 4;
   c->GPRx = (uintptr_t)arg;
   c->pdir = NULL;
--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -95,7 +95,6 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *c = (Context *)(kstack.end - sizeof(Context));
   c->mstatus = 0x00000080;
-  printf("entry = %p\n", entry);
   c->mepc = (uintptr_t)entry - 4;
   c->pdir = as->ptr;
   c->np = 1;
