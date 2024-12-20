d9c0b2077f80f69c3144708babacc86df02f455e
tracer-ics2024 <tracer@njuics.org>
2024-12-20 15:43:40 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:43:40 up  1:42,  2 users,  load average: 0.43, 0.40, 0.37

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -31,12 +31,11 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr.mstatus = (cpu.csr.mstatus & ~MSTATUS_MIE);
   cpu.csr.mepc = epc;
   cpu.csr.mcause = NO;
-  printf("sp : %x\n", cpu.gpr[2]);
   return cpu.csr.mtvec;
 }
 
 word_t isa_query_intr() {
-  if (cpu.INTR && (cpu.csr.mstatus & MSTATUS_MIE)) {
+  if (cpu.INTR && (cpu.csr.mstatus & MSTATUS_MIE) && false) {
     cpu.INTR = false;
     return IRQ_TIMER;
   }
