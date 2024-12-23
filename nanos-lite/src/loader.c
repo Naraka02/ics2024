e34d118c9c51a01bf0217870567808a305820fbd
tracer-ics2024 <tracer@njuics.org>
2024-12-23 14:40:47 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  14:40:47 up 34 min,  1 user,  load average: 0.40, 0.47, 0.48

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -120,6 +120,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[],
 
   uintptr_t entry = loader(pcb, filename);
   Area kstack = {pcb->stack, pcb->stack + STACK_SIZE};
+  printf("kstack: %p, %p\n", kstack.start, kstack.end);
   pcb->cp = ucontext(&pcb->as, kstack, (void *)entry);
   pcb->cp->GPRx = (uintptr_t)(pcb->as.area.end - (stack_end - sp));
 }
