25bd71756da75093999694ff58450b0a63e23f1a
tracer-ics2024 <tracer@njuics.org>
2024-12-24 17:09:22 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 19 Dec 2024 21:29:01 +0000 x86_64 GNU/Linux  17:09:22 up  1:01,  1 user,  load average: 0.27, 0.17, 0.14

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ extern void __am_switch(Context *c);
 
 Context *__am_irq_handle(Context *c) {
   __am_get_cur_as(c);
-  printf("$sp = %x\n", c->gpr[2]);
+  printf("before switch $sp = %x\n", c->gpr[2]);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -30,7 +30,7 @@ Context *__am_irq_handle(Context *c) {
   if (c->mcause != 0x80000007) {
     c->mepc += 4;
   }
-  printf("$sp = %x\n", c->gpr[2]);
+  printf("after $sp = %x\n", c->gpr[2]);
   __am_switch(c);
   return c;
 }
