36a0405201f294e30c1d05c6f28d593b566d5f2e
tracer-ics2024 <tracer@njuics.org>
2024-12-24 17:19:31 +0800

    >  compile NEMU 231250124 赖涵 Linux myarch 6.12.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 19 Dec 2024 21:29:01 +0000 x86_64 GNU/Linux  17:19:31 up  1:11,  1 user,  load average: 0.47, 0.30, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -52,8 +52,6 @@ static inline int sys_gettimeofday(void *tv, void *tz) {
   uint64_t us = io_read(AM_TIMER_UPTIME).us;
   ((struct timeval *)tv)->tv_sec = us / 1000000;
   ((struct timeval *)tv)->tv_usec = us % 1000000;
-  printf("gettimeofday: %ld %ld\n", ((struct timeval *)tv)->tv_sec,
-         ((struct timeval *)tv)->tv_usec);
   return 0;
 }
 
